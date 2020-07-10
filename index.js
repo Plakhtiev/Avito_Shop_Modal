7ce29087b2c8ada4733f70ff77ef27e7425852d5
Plakhtiev Jaroslav <63554869+Plakhtiev@users.noreply.github.com>
2020-07-10 21:27:26 +0300

    Add image

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const dataBase = [];
+const dataBase = JSON.parse(localStorage.getItem('avito')) || [];
 
-const addAd = document.querySelector('.add__ad');
-const modalAdd = document.querySelector('.modal__add');
-const modalClose = document.querySelector('.modal__close');
-const modaBtnSubmit = document.querySelector('.modal__btn-submit');
-const modalSubmit = document.querySelector('.modal__submit');
-const catalog = document.querySelector('.catalog');
-const modalItem = document.querySelector('.modal__item');
-const elementsModalSubmit = [...modalSubmit.elements].filter(elem => elem.tagName !== 'BUTTON');
-const modalBtnWarning = document.querySelector('.modal__btn-warning');
+const addAd = document.querySelector('.add__ad'),
+    modalAdd = document.querySelector('.modal__add'),
+    modalClose = document.querySelector('.modal__close'),
+    modaBtnSubmit = document.querySelector('.modal__btn-submit'),
+    modalSubmit = document.querySelector('.modal__submit'),
+    catalog = document.querySelector('.catalog'),
+    modalItem = document.querySelector('.modal__item'),
+    modalBtnWarning = document.querySelector('.modal__btn-warning'),
+    modalFileInput = document.querySelector('.modal__file-input'),
+    modalFileBtn = document.querySelector('.modal__file-btn'),
+    modalImageAdd = document.querySelector('.modal__image-add');
 
-const checkForm = () => { // валидация формы
+const textFileBtn = modalFileBtn.textContent;
+const srcModalImg = modalImageAdd.src;
+
+const elementsModalSubmit = [...modalSubmit.elements]
+    .filter(elem => elem.tagName !== 'BUTTON');
+
+const infoPhoto = {};
+
+const saveDB = () => localStorage.setItem('avito', JSON.stringify(dataBase));
+
+const checkForm = () => { // валидация формы на заполнение
     const validForm = elementsModalSubmit.every(elem => elem.value);
     modaBtnSubmit.disabled = !validForm;
     modalBtnWarning.style.display = validForm ? 'none' : '';
@@ -23,13 +35,34 @@ const closeModal = function(event) { // закрытие модальных ок
         modalAdd.classList.add('hide');
         document.removeEventListener('keydown', closeModal);
         modalSubmit.reset();
+        modalFileBtn.textContent = textFileBtn; // очистка кнопки "Добавить"
+        modalImageAdd.src = srcModalImg; // очистка картинки
         checkForm();
     }
 };
 
+modalFileInput.addEventListener('input', event => {
+    const target = event.target;
+    const reader = new FileReader();
+    const file = target.files[0];
+    infoPhoto.filename = file.name; // добавляем имя и размер файла
+    infoPhoto.size = file.size;
+    reader.readAsBinaryString(file); // одслеживаем файл 
+
+    reader.addEventListener('load', event => {
+        if (infoPhoto.size < 200000) {
+            modalFileBtn.textContent = infoPhoto.filename;
+            infoPhoto.base64 = btoa(event.target.result);
+            modalImageAdd.src = `data:image/jpeg;base64,${infoPhoto.base64}`; // добавляем фото
+        } else {
+            modalFileBtn.textContent = 'Файл не должен превышать 200кб';
+        }
+    });
+});
+
 modalSubmit.addEventListener('input', checkForm);
 
-modalSubmit.addEventListener('submit', event => {
+modalSubmit.addEventListener('submit', event => { // отправка формы
     event.preventDefault();
     const itemObj = {};
     for (const elem of elementsModalSubmit) {
@@ -37,7 +70,7 @@ modalSubmit.addEventListener('submit', event => {
     }
     dataBase.push(itemObj);
     closeModal({ target: modalAdd });
-    console.log(dataBase);
+    saveDB();
 });
 
 addAd.addEventListener('click', () => {
@@ -48,7 +81,6 @@ addAd.addEventListener('click', () => {
 
 modalAdd.addEventListener('click', closeModal);
 modalItem.addEventListener('click', closeModal);
-
 
 
 catalog.addEventListener('click', event => {
